76bbd4c66025bc41aad9a868cf90de82c3536d26
Takuya Mukohira <takuya.mk96@gmail.com>
2018-11-19 15:36:21 +0900

    Initial Commit

---
+++ OyatsuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OyatsuApp
+//
+//  Created by Takuya Mukohira on 2018/11/19.
+//  Copyright © 2018 Takuya Mukohira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
